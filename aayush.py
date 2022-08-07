2a242f399f14edd8e3eb523b7f41db8c3da6fce0
Sincryptaayush <108784856+Sincryptaayush@users.noreply.github.com>
2022-08-07 12:48:38 +0530

    Add files via upload

---
+++ aayush.py
@@ -0,0 +1,34 @@
+#sincryption
+#scriptedbysincryptcra5h
+print('''     #       #    #     # #     #  #####  #     # 
+   # #     # #    #   #  #     # #     # #     # 
+  #   #   #   #    # #   #     # #       #     # 
+ #     # #     #    #    #     #  #####  ####### 
+ ####### #######    #    #     #       # #     # 
+ #     # #     #    #    #     # #     # #     # 
+ #     # #     #    #     #####   #####  #     # 
+                                                 ''')
+                                                 
+print('\n')
+
+print(' hello dear ! welcome to small game tool')
+print('\n')
+
+path_1=input(" enter your name to start the game ----> ")
+print('\n')
+base=input(' enter the password to login in loop game tool :- ' )
+print('\n')
+if base==('samay'):
+   print(f'Dear {path_1} you are successfully logged in loop game tool ')
+else:
+	print(f' sorry dear {path_1} you have entered wrong password ')
+	exit()
+print('\n')
+print(f' ok dear {path_1} so this is the game of loop ! ')
+print('\n')
+zara=(input(' enter the sentence you want in loop ------> '))
+print('\n')
+samay=int(input(' enter how many time you want to print ------>>>> '))
+
+for aayush in range(samay+1):
+ print(f'{zara}  '+str(aayush))
